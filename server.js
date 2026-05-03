430e36770c15ea1c15a6836c019dc419a0950fe8
regazo <z.mbame11@gmail.com>
2026-05-03 17:13:03 +0100

    Implemented login and user creation

--- server.js
+++ server.js
@@ -15,4 +15,28 @@ db = client.db("cloud");
 
 console.log("db conected...");
 
+// LOGIN ROUTE
+app.post("/login", async (req,res)=>{
+  const { uid, email } = req.body;
+
+  let user = await db.collection("users").findOne({_id:uid});
+
+  if(!user){
+    await db.collection("users").insertOne({
+      _id: uid,
+      email: email
+    });
+
+    await db.collection("directories").insertOne({
+      _id: "root-" + uid,
+      userId: uid,
+      name: "/",
+      path: "/",
+      parent: null
+    });
+  }
+
+  res.send("ok");
+});
+
 app.listen(3000, () => console.log("server running"));
