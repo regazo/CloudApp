5a7acbc36c7568e3f89851fcc3984bd7f87bace9
regazo <z.mbame11@gmail.com>
2026-05-03 17:20:39 +0100

    Added directory creation and deletion

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+//zakarea erezzaghi 3074880
 import express from "express";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
 
@@ -10,12 +12,13 @@ app.use(express.json());
 const client = new MongoClient(process.env.MONGO_URI);
 let db;
 
+// connect db
 await client.connect();
 db = client.db("cloud");
 
 console.log("db conected...");
 
-// LOGIN ROUTE
+// login + makeuser
 app.post("/login", async (req,res)=>{
   const { uid, email } = req.body;
 
@@ -27,8 +30,9 @@ app.post("/login", async (req,res)=>{
       email: email
     });
 
+    // make root folder
     await db.collection("directories").insertOne({
-      _id: "root-" + uid,
+      _id: "root-"+uid,
       userId: uid,
       name: "/",
       path: "/",
@@ -37,6 +41,36 @@ app.post("/login", async (req,res)=>{
   }
 
   res.send("ok");
+});
+
+// creates the folder
+app.post("/mkdir", async (req,res)=>{
+  const {userId,name,path} = req.body;
+
+  await db.collection("directories").insertOne({
+    _id:uuidv4(),
+    userId,
+    name,
+    path:path+name+"/",
+    parent:path
+  });
+
+  res.send("dir made");
+});
+
+// delete folde
+app.post("/rmdir", async (req,res)=>{
+  const {path} = req.body;
+
+  const f = await db.collection("files").findOne({path});
+  const d = await db.collection("directories").findOne({parent:path});
+
+  if(f || d){
+    return res.send("not empty");
+  }
+
+  await db.collection("directories").deleteOne({path});
+  res.send("deleted");
 });
 
 app.listen(3000, () => console.log("server running"));
