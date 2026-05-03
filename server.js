ac378da5e169862eb0bf5b83555a82b6e6d834c9
regazo <z.mbame11@gmail.com>
2026-05-03 17:07:48 +0100

    Added MongoDB connection

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+
+const client = new MongoClient(process.env.MONGO_URI);
+let db;
+
+await client.connect();
+db = client.db("cloud");
+
+console.log("db conected...");
 
 app.listen(3000, () => console.log("server running"));
